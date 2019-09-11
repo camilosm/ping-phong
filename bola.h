71eec89beddbd51de30295493ef16398d440c41e
camilosm <camilocsm@gmail.com>
2019-09-11 19:24:36 -0300

    corrigidos os typedef das structs

--- bola.h
+++ bola.h
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 
-struct bola{
+typedef struct bola{
 	float raio;
 	float x;
 	float y;
 	float vx;
 	float vy;
-}
-
-typedef struct bola bola;
+} bola;
 
 void mover(bola *bola);
 void mudar_tamanho(bola *bola, float fator);
--- jogador.h
+++ jogador.h
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct jogador{
+typedef struct jogador{
 	bool lado; //true esquerda, false direita
 	float y; //altura do centro do jogador
 	unsigned int pontos;
-};
-
-typedef struct jogador jogador;
+} jogador;
 
 void mover(jogador *jogador, float distancia);
 void pontuar(jogador *jogador, int pontos);
